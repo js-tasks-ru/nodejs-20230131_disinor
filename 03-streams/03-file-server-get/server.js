3866e52a176bd666c5046afdbb8f8fbf149f79a1
aleksandrpikulin <apikulin437@gmail.com>
2023-02-11 20:12:29 +0400

    решение 03-03

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,7 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
@@ -9,9 +9,27 @@ server.on('request', (req, res) => {
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
-
   switch (req.method) {
     case 'GET':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Nested paths are not supported');
+        break;
+      }
+      if (!fs.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end('File not found');
+        break;
+      }
+      const stream = fs.createReadStream(filepath);
+      stream.on('error', (err) => {
+        res.statusCode = 500;
+        res.end('Server error');
+      });
+      res.on('aborted', () => {
+        stream.destroy();
+      });
+      stream.pipe(res);
 
       break;
 
