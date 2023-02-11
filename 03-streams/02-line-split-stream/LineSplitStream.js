e9ba6e1fa36c7875a2b04d7e43917fff6cfac5ab
aleksandrpikulin <apikulin437@gmail.com>
2023-02-11 23:36:40 +0400

    refactor 03-02

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,54 +1,33 @@
-const stream = require('stream');
+const { Transform } = require('stream');
 const os = require('os');
 
-class LineSplitStream extends stream.Transform {
+class LineSplitStream extends Transform {
   constructor(options) {
     super(options);
-    this.str = '';
-    this.buffer = '';
-    this.bufferAfterWriteStr = '';
-    this.arr = [];
+    this.encoding = options.encoding || 'utf-8';
+    this.remaining = '';
   }
 
   _transform(chunk, encoding, callback) {
-    const chunkStr = chunk.toString();
-    if (chunkStr.includes(os.EOL)) {
-      this.arr = chunkStr.split(os.EOL);
-      this.bufferAfterWriteStr = this.arr.pop();
-      if (this.buffer) {
-        this.str = this.buffer + chunkStr.split(os.EOL)[0];
-        this.bufferAfterWriteStr = chunkStr.split(os.EOL).pop();
-        this.buffer = chunkStr.split(os.EOL).pop();
-      } else {
-        if (this.arr.length > 2) {
-          this.arr.forEach(el => {
-            this.emit('data', el);
-          });
-          callback(null);
-          return;
-        } else {
-          this.str = chunkStr.split(os.EOL)[0];
-          this.bufferAfterWriteStr = chunkStr.split(os.EOL).pop();
-        }
-      }
-      if (this.str) {
-        const res = this.str;
-        this.str = '';
-        callback(null, res);
-      }
-    } else {
-      this.str = chunkStr.split(os.EOL)[0];
-      this.buffer = chunkStr.split(os.EOL).pop();
-      callback();
+    const chunkString = chunk.toString(this.encoding);
+    const lines = chunkString.split(os.EOL);
+    lines[0] = this.remaining + lines[0];
+
+    this.remaining = lines.pop();
+
+    for (const line of lines) {
+      this.push(line);
     }
+
+    callback();
   }
 
   _flush(callback) {
-    if ( this.arr.length > 2 ) {
-      callback(null, this.bufferAfterWriteStr);
-      return;
+    if (this.remaining) {
+      this.push(this.remaining);
     }
-    callback(null, this.bufferAfterWriteStr + this.buffer);
+
+    callback();
   }
 }
 
